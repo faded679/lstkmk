316140772e974718b43b78f2aca5adabebdaff3e
Your Name <your@email.com>
2026-06-03 15:04:56 +0300

    Обновлен счетчик Яндекс.Метрики на 109614555

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,20 +104,13 @@ export default function RootLayout({
                 if (document.scripts[j].src === r) { return; }
               }
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109614135', 'ym');
-            ym(109614135, 'init', {
-              ssr:true,
-              webvisor:true,
-              clickmap:true,
-              ecommerce:"dataLayer",
-              accurateTrackBounce:true,
-              trackLinks:true
-            });
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109614555', 'ym');
+            ym(109614555, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
           `}
         </Script>
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/109614135" style={{position:'absolute', left:'-9999px'}} alt="" />
+            <img src="https://mc.yandex.ru/watch/109614555" style={{position:'absolute', left:'-9999px'}} alt="" />
           </div>
         </noscript>
       </body>
