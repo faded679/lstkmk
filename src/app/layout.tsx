00734d54a83efeaa9c35f3977b2ddd30bb6d77da
radar <66486286+radardesign@users.noreply.github.com>
2026-06-23 13:28:01 +0300

    feat: add second Yandex Metrica counter 110082764

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,13 +130,15 @@ export default function RootLayout({
                 if (document.scripts[j].src === r) { return; }
               }
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109614555', 'ym');
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
             ym(109614555, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            ym(110082764, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
           `}
         </Script>
         <noscript>
           <div>
             <img src="https://mc.yandex.ru/watch/109614555" style={{position:'absolute', left:'-9999px'}} alt="" />
+            <img src="https://mc.yandex.ru/watch/110082764" style={{position:'absolute', left:'-9999px'}} alt="" />
           </div>
         </noscript>
       </body>
