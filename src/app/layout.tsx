5908c9117c35da2701490cef1d19b92d0266b07d
Vanyusha <faded679@users.noreply.github.com>
2026-06-26 14:04:23 +0200

    fix: add webvisor:true to Yandex Metrika 110082764 init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -139,7 +139,7 @@ export default function RootLayout({
               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
             })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            ym(110082764, "init", {ssr:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            ym(110082764, "init", {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
           `}
         </Script>
         <noscript><div><img src="https://mc.yandex.ru/watch/110082764" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
