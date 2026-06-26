962f833b21c82e02cdfb3cb6bea978e785717ef3
Vanyusha <faded679@users.noreply.github.com>
2026-06-26 18:38:00 +0200

    Убрал Яндекс Метрику из проекта полностью

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,12 +109,6 @@ export default function RootLayout({
         {/* Preload критических шрифтов */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        {/* Yandex.Metrika counter */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(110082764,"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true,ecommerce:"dataLayer"});`,
-          }}
-        />
       </head>
       <body>
         <OrganizationSchema />
@@ -134,16 +128,6 @@ export default function RootLayout({
             gtag('config', 'G-KR3X1J843D');
           `}
         </Script>
-        {/* Yandex.Metrika noscript */}
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/110082764"
-              style={{ position: "absolute", left: "-9999px" }}
-              alt=""
-            />
-          </div>
-        </noscript>
       </body>
     </html>
   );
