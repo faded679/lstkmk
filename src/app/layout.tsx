a80571853907ec2c99405fd41bed7a24b7fa242e
Your Name <your@email.com>
2026-06-26 19:41:09 +0300

    add: Yandex Metrika 110082764 at start of body with beforeInteractive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,6 +111,19 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body>
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="beforeInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110082764', 'ym');
+            ym(110082764, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/110082764" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
         <OrganizationSchema />
         <WebSiteSchema />
         <BreadcrumbSchema />
